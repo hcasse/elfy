7cac181971dd0a17b98e8c2c9a99e885fe49e1fa
casse <>
2012-11-18 08:52:51 +0100

    First version, with icon manager.

---
+++ src/elf/ui/IconManager.java
@@ -0,0 +1,68 @@
+/*
+ * Orchid Framework
+ * Copyright (c) 2009 - Hugues Cassé <hugues.casse@laposte.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package elf.ui;
+
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+
+// VERSION=1.0
+
+/**
+ * Handle a set of icons.
+ * @author casse
+ */
+public class IconManager {
+	URL path;
+	Hashtable<String, Icon> icons = new Hashtable<String, Icon>();
+	
+	/**
+	 * Build the icon manager.
+	 * @param path		Path to get the icon from.		
+	 */
+	public IconManager(URL path) {
+		this.path = path;
+	}
+	
+	/**
+	 * Get the icon for the given name. Look for a file named path / name ".png".
+	 * @param name		Name of the icon.
+	 * @return			Found icon or a null icon.
+	 */
+	public Icon get(String name) {
+		Icon icon = icons.get(name);
+		if(icon == null) {
+			try {
+				URL url = new URL(path.toString() + "/" + name + ".png");
+				Image image = ImageIO.read(url);
+				icon = new Icon.Image(image);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				icon = Icon.NULL;
+			} catch (IOException e) {
+				icon = Icon.NULL;
+			}
+			icons.put(name, icon);
+		}
+		return icon;
+	}
+}
